fb7de41ec882b9fbdcc1491ef80c908a77848de4
kwiatkowskijan <70690975+kwiatkowskijan@users.noreply.github.com>
2023-12-10 01:46:08 +0100

    idk

--- Assets/scripts/EnemiesSpawner.cs
+++ Assets/scripts/EnemiesSpawner.cs
@@ -5,31 +5,32 @@ using UnityEngine.Tilemaps;
 
 public class EnemiesSpawner : MonoBehaviour
 {
-
     public GameObject enemy1;
     public GameObject enemy2;
     public Tilemap tilemap;
+    public float spawnDelay = 2.5f;
+
     private void OnTriggerEnter2D(Collider2D collision)
-    {   
-        for (int i = 0; i < 3; i++)
+    {
+        if (collision.CompareTag("Player"))
         {
-            if (collision.CompareTag("Player"))
-            {
-                Debug.Log("DUPA");
-
-                Vector3Int randomCell = new Vector3Int(
-                    Random.Range(tilemap.cellBounds.xMin + 2, tilemap.cellBounds.xMax - 2),
-                    Random.Range(tilemap.cellBounds.yMin + 2, tilemap.cellBounds.yMax - 2),
-                    0
-                );
-
-                Vector3 spawnPosition = tilemap.CellToWorld(randomCell);
-
-
-                Instantiate(enemy1, spawnPosition, Quaternion.identity);
-
-            }
+            Invoke("SpawnEnemy", spawnDelay);
         }
-     
+    }
+
+    void SpawnEnemy()
+    {
+        for (int i = 0; i < Random.Range(1, 10); i++)
+        {
+            Vector3Int randomCell = new Vector3Int(
+                Random.Range(tilemap.cellBounds.xMin + 3, tilemap.cellBounds.xMax - 3),
+                Random.Range(tilemap.cellBounds.yMin + 3, tilemap.cellBounds.yMax - 3),
+                0
+            );
+
+            Vector3 spawnPosition = tilemap.CellToWorld(randomCell);
+
+            Instantiate(enemy1, spawnPosition, Quaternion.identity);
+        }
     }
 }
